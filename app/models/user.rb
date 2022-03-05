3f488e261523832b18f465f4cc523937824812e3
Cooperjuan <jcooper8@chicagobooth.edu>
2022-03-05 19:32:49 +0000

    Beginning

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string
+#  first_name      :string
+#  home_location   :string
+#  last_name       :string
+#  password_digest :string
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, :presence => true
+  has_secure_password
+end
